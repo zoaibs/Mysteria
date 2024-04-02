8fa518731d2ddb208d9032c7efa84fb9b0293088
zoaib <zoaibsihorwala06@gmail.com>
2024-04-02 01:23:15 -0400

    hi

--- sign.gd
+++ sign.gd
@@ -3,6 +3,7 @@ extends Node2D
 
 @export var text_display: String
 @onready var tbox = $TextboxContainer/MarginContainer/HBoxContainer/Label
+@onready var text_container = $TextboxContainer
 var is_visible = false
 
 
@@ -10,6 +11,6 @@ var is_visible = false
 func _on_area_2d_body_entered(body):
 	if body.is_in_group("Player"):
 		print("texbox displayed")
-		self.visible = true
+		text_container.visible = true
 		tbox.text = text_display
 	pass # Replace with function body.
