cfbc558851615a7f6919f4e0364567ce3aaf1711
zoaib <zoaibsihorwala06@gmail.com>
2024-04-02 01:45:58 -0400

    add sign functionality

--- sign.gd
+++ sign.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends CharacterBody2D
 
 
 @export var text_display: String
@@ -13,4 +13,13 @@ func _on_area_2d_body_entered(body):
 		print("texbox displayed")
 		text_container.visible = true
 		tbox.text = text_display
+	pass # Replace with function body.
+
+
+func _on_area_2d_body_exited(body):
+	if body.is_in_group("Player"):
+		print("texbox hidden")
+		text_container.visible = false
+		
+	pass # Replace with function body.
 	pass # Replace with function body.
