6b72005dab3abf7cfdffe2fd5d4140d124af2243
AdvaithBura <AdvaithBura@gmail.com>
2024-04-03 01:06:28 -0400

    boss fire changes

--- fire.gd
+++ fire.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 var rng = RandomNumberGenerator.new()
+var fire = false
 var fireList= [$"../fire1",$"../fire2",$"../fire3",$"../fire4",$"../fire5",$"../fire6",$"../fire7",$"../fire8"]
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -25,11 +26,12 @@ func _process(delta):
 		$"../fire7".visible=true
 		$"../fire8".visible=true
 	
+	
 
 
 
 func _on_area_2d_body_entered(body):
-	if body.name == "Player" and self.visible:
+	if body.name == "Player" and self.visible and fire:
 		body.take_damage()
 		
 func thereset():
@@ -41,3 +43,17 @@ func thereset():
 		$"../fire6".visible=false
 		$"../fire7".visible=false
 		$"../fire8".visible=false
+
+
+func _on_animation_player_animation_finished(anim_name):
+	if anim_name == "fire_moving":
+		$AnimationPlayer.play("no_fire")
+		fire=false
+	else:
+		$AnimationPlayer.play("fire_moving")
+		fire=true
+		
+
+
+func _on_area_2d_body_exited(body):
+	pass # Replace with function body.
